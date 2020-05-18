23b7be810efd7eba991f046feb100d37178fe2bb
Bruce Zheng <bruce.c.zheng@gmail.com>
2020-05-17 21:49:27 -0700

    Factor some date utils into own file

---
+++ dateUtils.js
@@ -0,0 +1,50 @@
+const monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November', 
+  'December',
+];
+
+const monthAbbreviations = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+
+function getMonthDayString(date) {
+  let dateSuffix = 'th';
+  switch (date.getDate()) {
+    case 1:
+    case 21:
+    case 31:
+      dateSuffix = 'st';
+      break;
+    case 2:
+    case 22:
+      dateSuffix = 'nd';
+      break;
+    case 3:
+    case 23:
+      dateSuffix = 'rd';
+      break;
+  }
+  return `${monthAbbreviations[date.getMonth()]}` + 
+             ` ${date.getDate()}${dateSuffix}`;
+}
